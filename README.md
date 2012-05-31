acb603c5bb8756f85d1fab83a671d3ef7f3d6686
David Poisl <david@poisl.at>
2012-05-31 16:55:38 +0200

    added MANIFEST.in, updated README.md and setup.py

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-pySamsung
-=========
+samsung
+=======
 
-network remote library for samsung C, D and E-series devices
+Network remote control library for Samsung C, D and E-series devices.
 
-This is currently under heavy construction
+This library supports sending remote keycodes and tests to Samsung
+network-enabled home entertainment devices (eG TV sets, BluRay/DVD 
+player, home theater systems, ...).
+
+This library is currently undergoing rewriting and neither the API
+nor the functionality will stay the same.
--- setup.py
+++ setup.py
@@ -2,24 +2,28 @@
 
 from distutils.core import setup
 
+readme = open("README.md")
+
 setup(name="samsung", 
       version="0.1.0",
       description='network remote library for Samsung C, D and E-Series devices',
+      long_description=readme.read(),
       author='David Poisl',
       author_email='david@poisl.at',
       url='https://github.com/dpoisl/pySamsung/',
+      platforms=("any",)
       packages=('samsung',),
-      scripts=("bin/samsung_remote",),
-      classifiers=(
-          "Development Status :: 3 - Alpha",
-          "Intended Audience :: Developers",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3",
-          "Operating System :: OS Independent",
-          "Topic :: Software Development :: Libraries",
-          "Topic :: Multimedia",
-          "Topic :: Home Automation",
-          "Topic :: Internet",
-          )
+      scripts=("scripts/samsung_remote",),
+      classifiers=("Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Operating System :: OS Independent",
+                   "Topic :: Software Development :: Libraries",
+                   "Topic :: Multimedia",
+                   "Topic :: Home Automation",
+                   "Topic :: Internet",
+                   )
      )
-      
+
+readme.close()
